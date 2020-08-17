3fb17a959d4af662e960ca76e502520db9928607
Nathan Alam <nalam@NAlam-MBP.fios-router.home>
2020-08-17 16:30:33 -0400

    Port over Ginkgo Form Builder

---
+++ example/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PlaygroundContainer from "./PlaygroundContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <PlaygroundContainer title="React JSON Schema Form Builder" />
+      </body>
+    </div>
+  );
+}
+
+export default App;
